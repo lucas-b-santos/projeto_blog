0b31a1f63ce2cba2ff8dbc44d9c69db375fa2dd2
Lucas Batista <72418534+ChrolloL@users.noreply.github.com>
2022-08-02 18:25:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('button')
+const face = document.getElementById('face')
+const insta = document.getElementById('insta')
+const mail = document.getElementById('mail')
+
+btn.addEventListener('click', () => {
+    window.open('https://www.google.com.br/', '_blank')
+})
+
+face.addEventListener('click', () => {
+    window.open('https://www.google.com.br/', '_blank')
+})
